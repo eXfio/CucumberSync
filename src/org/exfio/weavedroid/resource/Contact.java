8c60dcfd912411c715fbfbb36f27206653141e1d
Gerry Healy <nickel_chrome@mac.com>
2014-10-12 15:55:00 +1100

    Updated copyright headers

--- src/org/exfio/weavedroid/resource/Contact.java
+++ src/org/exfio/weavedroid/resource/Contact.java
@@ -1,9 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2013 Richard Hirner (bitfire web engineering).
+ * Copyright (c) 2014 Gerry Healy <nickel_chrome@mac.com>
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Based on DavDroid:
+ *     Richard Hirner (bitfire web engineering)
+ * 
+ * Contributors:
+ *     Gerry Healy <nickel_chrome@mac.com> - Initial implementation
  ******************************************************************************/
 package org.exfio.weavedroid.resource;
 
